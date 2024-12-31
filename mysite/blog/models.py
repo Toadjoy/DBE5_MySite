e38d5db0cf0588fe1d137155d6f54110a28312bb
Toadjoy <cwhuman66@gmail.com>
2024-12-30 20:10:36 -0800

    Add in django-taggit, update models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.conf import settings
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -57,6 +58,8 @@ class Post(models.Model):
                               self.publish.month,
                               self.publish.day,
                               self.slug])
+    
+    tags = TaggableManager()
     
 
 class Comment(models.Model):
